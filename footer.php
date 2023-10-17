8fa673076c6a4a47b7536a2c52a7cb7819415ee3
Sandra Höst Kannerberg <sandra.hkannerberg@gmail.com>
2023-10-17 10:56:44 +0200

    css footer

--- footer.php
+++ footer.php
@@ -1,7 +1,7 @@
 <footer class="container-fluid text-center">
     <div class="row">
         <div>
-            Test Assignment - Copyright &copy; Sandra Höst Kannerberg 2023
+            <span class="my-footer-text">Test Assignment - Copyright &copy; Sandra Höst Kannerberg 2023</span>
         </div>
     </div>
 </footer>
--- header.php
+++ header.php
@@ -1,20 +1,18 @@
 <header class="my-header container-fluid">
-
-        <div class="my-header-wrapper row">
-            <div class="my-title-wrapper col">
-                <h1><a class="navbar-brand" href="index.php">Test</a></h1>
-            </div>
-      
-            <div class="my-menu-wrapper col">
-                <ul class="my-menu col-4">
-                    <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="index.php">Home</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="excistingUserPage.php">Users</a>
-                    </li>
-                </ul>
-            </div>
+    <div class="my-header-wrapper row">
+        <div class="my-title-wrapper col">
+            <h1><a class="navbar-brand" href="index.php">Test</a></h1>
         </div>
-
+      
+        <div class="my-menu-wrapper col">
+            <ul class="my-menu col-4">
+                <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="index.php">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="excistingUserPage.php">Users</a>
+                </li>
+            </ul>
+        </div>
+    </div>
 </header>
