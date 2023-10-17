af10effd9ebd25433b131ba5a2f598b2f5b1904b
Sandra Höst Kannerberg <sandra.hkannerberg@gmail.com>
2023-10-17 15:13:42 +0200

    add icon, working with css userlist

--- excistingUserPage.php
+++ excistingUserPage.php
@@ -3,22 +3,25 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Link Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <!-- Link to my CSS-file -->
+    <link rel="stylesheet" type="text/css" href="css/style.css">
     <link rel="stylesheet" type="text/css" href="css/user.css">
+    <script src="https://kit.fontawesome.com/bce314e193.js" crossorigin="anonymous"></script>
     <title>Excisting users</title>
 </head>
 <body>
 
-    <?php include('header.php'); ?>
+    <?php include("header.php"); ?>
 
-    <main class="wrapper-users">
-        <div class='users-container'>
-            <?php include('includes/userList.php'); ?>
+    <main class="my-userlist-wrapper container-fluid">
+        <div class="users-container row">
+            <?php include("includes/userList.php"); ?>
         </div>
     </main>
 
-    <?php include('footer.php'); ?>
+    <?php include("footer.php"); ?>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 </body>
 </html>
--- includes/userList.php
+++ includes/userList.php
@@ -1,4 +1,4 @@
-<h2>Existing users</h2>
+<h2 class="text-center">Existing users</h2>
 
 <table class="table">
   <thead>
@@ -6,6 +6,8 @@
       <th scope="col">ID</th>
       <th scope="col">Name</th>
       <th scope="col">Country</th>
+      <th scope="col">Edit</th>
+      <th scope="col">Delete</th>
     </tr>
   </thead>
 
@@ -28,14 +30,16 @@
     //Loop all users and give each user one line
     if ($users->num_rows > 0) {
         while ($row = $users->fetch_assoc()) {
-            echo '<tbody>';
+            echo '<tbody class="my-tablebody">';
             echo '<tr>';
             echo '<td>' . $row['id'] . '</td>';
             echo '<td>' . $row['name'] . '</td>';
             echo '<td>' . $row['country'] . '</td>';
             echo '<td>';
-            echo '<button type="button" class="btn btn-dark">Edit</button>';
-            echo '<button type="button" class="btn btn-danger">Delete</button>';
+            echo '<button type="button" class="my-edit-btn btn btn-dark"><i class="fa-solid fa-pen-to-square"></i></button>';
+            echo '</td>';
+            echo '<td>';
+            echo '<button type="button" class="my-del-btn btn btn-danger"><i class="fa-solid fa-trash"></i></button>';
             echo '</td>';
             echo '</tr>';
             echo '</tbody>';
