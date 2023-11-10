a94b35a39f0aa8fed7f4c0230cd690d5370851e0
Sandra Höst Kannerberg <sandra.hkannerberg@gmail.com>
2023-11-10 13:12:12 +0100

    update connection.php

--- connection.php
+++ connection.php
@@ -1,15 +1,15 @@
 <?php
-//Connect to database from the Docker container
-$servername = 'db';
-$username = 'root';
-$password = 'test';
-$database = 'test-uppgift';
+$servername = "172.22.64.1";
+$username = "root";
+$password = "test";
+$database = "test-uppgift";
 $port = 3307;
 
+// Connect to database
 $conn = mysqli_connect($servername, $username, $password, $database, $port);
 
-//Check the connection
+// Check connection
 if ($conn->connect_error) {
-    die("Couldn't connect to database: " . $conn->connect_error);
+    die("Couldn't connect to the database: " . $conn->connect_error);
 }
 ?>
